068c62d7f812f166636c4e5a10e0519a4171fa43
Piraveen Kandiah <piraveen.kandiah@student.uva.nl>
2023-01-25 10:46:12 +0100

    Update: Fire + Extuingisher
    Particles and scripts are implemented.

---
+++ OSE/Assets/Scripts/Fire.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField, Range(0f, 1.0f)] private float currentIntensity = 1.0f;
+    private float [] startIntensities = new float[0];
+
+    [SerializeField] private ParticleSystem [] fireParticleSystems = new ParticleSystem[0];
+    
+    float timeLastWatered = 0;
+    [SerializeField] private float regenDelay = 2.5f;
+    [SerializeField] private float regenRate = 0.1f;
+    
+    private bool isLit = true;
+
+    private void Start()
+    {
+        startIntensities = new float[fireParticleSystems.Length];
+
+        for (int i = 0; i < fireParticleSystems.Length; i++) 
+        {
+            startIntensities[i] = fireParticleSystems[i].emission.rateOverTime.constant;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        if (isLit && currentIntensity < 1.0f && Time.time - timeLastWatered >= regenDelay)
+        {
+            currentIntensity += regenRate * Time.deltaTime;
+            ChangeIntensity();
+        }
+    }
+
+    public bool TryExtinguish(float amount)
+    {
+        timeLastWatered = Time.time;
+        currentIntensity -= amount;
+        
+        ChangeIntensity();
+
+        if (currentIntensity <= 0 )
+        {
+            isLit = false;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void ChangeIntensity()
+    {
+        for (int i = 0; i < fireParticleSystems.Length; i++) 
+        {
+            var emission = fireParticleSystems[i].emission;
+            emission.rateOverTime =  currentIntensity * startIntensities[i];
+        }
+    }
+}
