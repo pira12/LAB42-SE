bbf270ea8e0f2cde7098c5782b8e70fbd2fb4a43
Yoshi Fu <yoshifu2002@gmail.com>
2023-01-30 16:22:05 +0100

    changed camera controller to continuous turn and fire extinguishers only extiniguishers fires of the same type.

--- OSE/Assets/Scripts/Extinguisher.cs
+++ OSE/Assets/Scripts/Extinguisher.cs
@@ -2,16 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BlusserType
+{
+    co2,
+    metaal,
+    poeder,
+    schuim,
+    vet
+}
+
 public class Extinguisher : MonoBehaviour
 {
     [SerializeField]
     private float amountExtinguishedPerSecond = 0.3f;
 
+    [SerializeField]
+    private Lever lever;
+
+    [SerializeField]
+    private BlusserType type;
+
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out RaycastHit hit, 100f) && hit.collider.TryGetComponent(out Fire fire))
-            fire.TryExtinguish(amountExtinguishedPerSecond * Time.deltaTime);
+        Vector3 origin = gameObject.transform.position;
+        Vector3 direction = gameObject.transform.forward;
+
+        // Cast a ray from origin to direction and check for collision.
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, 100f) &&
+            hit.collider.TryGetComponent(out Fire fire))
+        {
+            // Check if the fire extinguisher is of the right type and used.
+            if (lever.leverSqueezed && fire.type == type)
+            {
+                fire.TryExtinguish(amountExtinguishedPerSecond * Time.deltaTime);
+            }
+        }
     }
 
 
