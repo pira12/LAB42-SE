59ce45959f27f736166b5b834cf0b0d427e89aac
Christiaan Molier <christiaan.molier@student.uva.nl>
2023-02-03 11:27:20 +0100

    Meeste duren gefixed hopelijk en score beter geformat

--- OSE/Assets/Scripts/ScoreCalculator.cs
+++ OSE/Assets/Scripts/ScoreCalculator.cs
@@ -16,10 +16,28 @@ public class ScoreCalculator : MonoBehaviour
 
     void Start()
     {
-        string alarm = StateManager.instance.alarmGeactiveert ? "v" : "x";
-        string brand = StateManager.instance.brandGeblust ? "v" : "x";
-        string brandblusser = StateManager.instance.correcteBrandblusserGebruikt ? "v" : "x";
+        string alarm = StateManager.instance.alarmGeactiveert ? "V" : "X";
+        string brand = StateManager.instance.brandGeblust ? "V" : "X";
+        string brandblusser = StateManager.instance.correcteBrandblusserGebruikt ? "V" : "X";
+
+        int total_score = 0;
+        
+        if (StateManager.instance.alarmGeactiveert)
+        {
+            total_score += 1;
+        }
+        
+        if (StateManager.instance.brandGeblust)
+        {
+            total_score += 1;
+        }
+        
+        if (StateManager.instance.correcteBrandblusserGebruikt)
+        {
+            total_score += 1;
+        }
+        
         scoreText.text =
-            $"Tijd: {StateManager.instance.tijd}s\n Brandalarm geactiveerd: {alarm}\n Brand geblust: {brand}\n Correcte brandblusser gebruikt: {brandblusser}";
+            $"Tijd: {StateManager.instance.tijd}s\n Brandalarm geactiveerd: {alarm}\n Brand geblust: {brand}\n Correcte brandblusser: {brandblusser}\n Score: {total_score}/3";
     }
 }
